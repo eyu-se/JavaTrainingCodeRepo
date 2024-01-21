d6ea9319a3d409fe2db7ca44910ae518ed028e0a
eyu-se <eyuelmulatu@gmail.com>
2024-01-21 22:02:43 +0300

    added write to file

--- src/et/com/inhousetraining/L12/util/FileData.java
+++ src/et/com/inhousetraining/L12/util/FileData.java
@@ -15,6 +15,13 @@ public class FileData {
 
     public static void main(String[] args) {
 
+        readFromFile();
+        //writeToFile();
+
+    }
+    
+    
+    static void readFromFile(){
         try {
             ReadFile rf = new ReadFile("/Users/eyu/Documents/test.txt");
             List<String> textData = rf.readFile();
@@ -28,6 +35,23 @@ public class FileData {
         } catch (IOException e) {
             System.out.println("Error reading from file");
         }
-
     }
+    
+    
+    static void writeToFile(){
+        try {
+            WriteFile wf = new WriteFile("/Users/eyu/Documents/test.txt", true);
+            
+            wf.writeToFile("Jhon Jack, Liam, Test");
+            
+        } catch (FileNotFoundException e) {
+            System.out.println("File was not found");
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+    
+    
+    
+    
 }
